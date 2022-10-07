feb01b1ae2d83eca3c25cdf0916709706d6ed3b6
jiho <jhkkim0114@gmail.com>
2022-10-07 21:57:56 +0900

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,114 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_dropzone/flutter_dropzone.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyAppState createState() => _MyAppState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyAppState extends State<MyApp> {
+  late DropzoneViewController controller1;
+  late DropzoneViewController controller2;
+  String message1 = 'Drop something here';
+  String message2 = 'Drop something here';
+  bool highlighted1 = false;
+  Uint8List? image;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  Widget buildZone1(BuildContext context) => Builder(
+    builder: (context) => DropzoneView(
+      operation: DragOperation.copy,
+      cursor: CursorType.grab,
+      onCreated: (ctrl) => controller1 = ctrl,
+      onLoaded: () => print('Zone 1 loaded'),
+      onError: (ev) => print('Zone 1 error: $ev'),
+      onHover: () {
+        setState(() => highlighted1 = true);
+        print('Zone 1 hovered');
+      },
+      onLeave: () {
+        setState(() => highlighted1 = false);
+        print('Zone 1 left');
+      },
+      onDrop: (ev) async {
+        print('Zone 1 drop: ${ev.name}');
+        final bytes = await controller1.getFileData(ev);
+        setState(() {
+          message1 = '${ev.name} dropped';
+          highlighted1 = false;
+          image = bytes;
+        });
+        print(bytes.sublist(0, 20));
+      },
+      onDropMultiple: (ev) async {
+        print('Zone 1 drop multiple: $ev');
+      },
+    ),
+  );
 
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
+  Widget build(BuildContext context) => MaterialApp(
+    home: Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Dropzone example'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              color: highlighted1 ? Colors.red : Colors.transparent,
+              child: Stack(
+                children: [
+                  buildZone1(context),
+                  Center(child: Text(message1)),
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          ),
+          Expanded(
+            child: Stack(
+              children: [
+                buildZone2(context),
+                Center(child: Text(message2)),
+                image != null ? Image.memory(image!) : Container()
+              ],
             ),
-          ],
-        ),
+          ),
+          ElevatedButton(
+            onPressed: () async {
+              print(await controller1.pickFiles(mime: ['image/jpeg', 'image/png']));
+            },
+            child: const Text('Pick file'),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+    ),
+  );
+
+  Widget buildZone2(BuildContext context) => Builder(
+    builder: (context) => DropzoneView(
+      operation: DragOperation.move,
+      onCreated: (ctrl) => controller2 = ctrl,
+      onLoaded: () => print('Zone 2 loaded'),
+      onError: (ev) => print('Zone 2 error: $ev'),
+      onHover: () => print('Zone 2 hovered'),
+      onLeave: () => print('Zone 2 left'),
+      onDrop: (ev) {
+        print('Zone 2 drop: ${ev.name}');
+        setState(() {
+          message2 = '${ev.name} dropped';
+        });
+        final stream = controller2.getFileStream(ev);
+        print(stream.take(20));
+      },
+      onDropMultiple: (ev) async {
+        print('Zone 2 drop multiple: $ev');
+      },
+    ),
+  );
 }
